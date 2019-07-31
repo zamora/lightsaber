ca23457aa64f614c07bb452d7c1b1920f8d91a4c
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2019-07-31 12:07:01 -0700

    Fix indentation

--- lightsaber.rkt
+++ lightsaber.rkt
@@ -371,17 +371,17 @@
      (inset (filled-rectangle 12 5 #:color "Red" #:border-width 2) 55 9 0 0))))
 
 (module+ main
- ; Test cases
- (lightsaber "DodgerBlue")
- (lightsaber "Lime" #:length 250)
- (lightsaber "Crimson" #:style 'vader)
- (lightsaber (make-color 144 67 202) #:style 'kylo)
- (lightsaber "DeepPink" #:style 'maul)
+  ; Test cases
+  (lightsaber "DodgerBlue")
+  (lightsaber "Lime" #:length 250)
+  (lightsaber "Crimson" #:style 'vader)
+  (lightsaber (make-color 144 67 202) #:style 'kylo)
+  (lightsaber "DeepPink" #:style 'maul)
 
- ; Let's make a double-bladed lightsaber!
- (define single (lightsaber "Gold" #:length 200 #:style 'maul))
- (define double (hc-append (flip-horizontal single) single))
- double)
+  ; Let's make a double-bladed lightsaber!
+  (define single (lightsaber "Gold" #:length 200 #:style 'maul))
+  (define double (hc-append (flip-horizontal single) single))
+  double)
 
 #|
 Thoughts
